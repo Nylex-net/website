5512c57b2c4d43a3098b77e7eb6f8005b0f96c23
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-09-01 11:59:16 -0700

    Working content editor.

--- DjangoTest/nylex/home/urls.py
+++ DjangoTest/nylex/home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), # path originally '/home' instead of ''.
+    # path('/home', views.home, name='home'),
 ]
--- DjangoTest/nylex/home/views.py
+++ DjangoTest/nylex/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import BlogPost
 
 def home(request):
-    return HttpResponse(loader.get_template('index.html').render())
+    content = BlogPost.objects.all()[0]
+    context = {
+        'content': content
+    }
+    return HttpResponse(loader.get_template('index.html').render(context, request))
