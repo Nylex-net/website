f98effe1cae46e05709480003b12d61dca5db7dc
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-09-06 10:09:34 -0700

    Change of content.

--- DjangoTest/nylex/home/models.py
+++ DjangoTest/nylex/home/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class BlogPost(models.Model):
+class Page(models.Model):
     title = models.CharField(max_length=200)
+    header = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField('date published')
--- DjangoTest/nylex/home/views.py
+++ DjangoTest/nylex/home/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import BlogPost
+from .models import Page
 
 def home(request):
-    content = BlogPost.objects.all()[0]
+    content = Page.objects.all()[0]
     context = {
         'content': content,
         'root': True
     }
     return HttpResponse(loader.get_template('home.html').render(context, request))
+
+def page(request, page_id):
+    content = Page.objects.get(pk=page_id)
+    context = {
+        'content': content,
+        'root': False
+    }
+    return HttpResponse(loader.get_template('template.html').render(context, request))
