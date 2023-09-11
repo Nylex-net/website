60ca556a7fdf9af32d6da4c3f3ef8e0bc2c0d6b1
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-09-11 11:27:06 -0700

    Resize of carousel

--- DjangoTest/nylex/home/static/js/scripts.js
+++ DjangoTest/nylex/home/static/js/scripts.js
@@ -2,12 +2,13 @@
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
-    if(window.scrollY >= 100) {
-        // header.classList.remove('bg-light');
+    if(window.scrollY >= 1) {
+        header.classList.remove('transparentNav');
         header.classList.add('bg-light');
     }
     else {
         header.classList.remove('bg-light');
+        header.classList.add('transparentNav');
         // header.classList.add('bg-light');
     }
 }
