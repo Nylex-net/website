26972a422825563f33b3ece820a8b7ae5ce73d5c
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-09-12 10:49:04 -0700

    meta tags for seo and customize meta description.

--- DjangoTest/nylex/home/models.py
+++ DjangoTest/nylex/home/models.py
@@ -6,11 +6,12 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 class Page(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=500)
     slug = models.SlugField(max_length=200, unique=True)
     header = models.CharField(max_length=200)
     banner = models.ImageField(upload_to='media')
     content = RichTextField()
-    pub_date = models.DateTimeField('date published')
+    # pub_date = models.DateTimeField('date published')
 
 def __str__(self):
         return self.title
