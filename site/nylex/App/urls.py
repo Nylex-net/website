825a7b16531a7c7926e7a8bf9ce209bfae7618d9
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-09-22 11:40:31 -0700

    Docked & adjusted code to run django's rest api.

--- site/nylex/App/urls.py
+++ site/nylex/App/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from App.views import page_view_set
+from rest_framework import routers
+
+# define the router
+router = routers.DefaultRouter()
+router.register(r'page', page_view_set) #the route tha will be used to access your API on the browser
 
 urlpatterns = [
-    path('', views.home, name='home'), # path originally '/home' instead of ''.
-    path('home', views.home, name='home'),
-    path('search', views.search, name='search'),
-    path('site-map', views.site_map, name='site-map'),
-    path('<slug:slug>', views.template, name='article')
+    path('', page_view_set.home, name='home'), # path originally '/home' instead of ''.
+    path('home', page_view_set.home, name='home'),
+    path('search', page_view_set.search, name='search'),
+    path('site-map', page_view_set.site_map, name='site-map'),
+    path('<slug:slug>', page_view_set.template, name='article'),
+    path('api-auth', include('rest_framework.urls'))
 ]
