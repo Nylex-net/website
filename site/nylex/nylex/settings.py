444bd63980723fef95e3a200fcf84af28ff97803
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-09-28 16:45:15 -0700

    Installs and configuration attempts for docker.

--- site/nylex/nylex/settings.py
+++ site/nylex/nylex/settings.py
@@ -123,13 +123,16 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -137,9 +140,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Define the directory where static files will be collected.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/vol/web/static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'App/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/vol/web/media')
 
 # Expire sessions after a specific time (e.g., 30 minutes)
 SESSION_COOKIE_AGE = 1800  # 30 minutes in seconds
