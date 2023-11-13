75353277dac9eb21164a4c09cbfa5f038d93fda8
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-11-13 11:28:17 -0800

    Working version of website using https.

--- site/proxy/run.sh
+++ site/proxy/run.sh
@@ -2,5 +2,19 @@
 
 set -e
 
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+echo "Checking for dhparams.pem"
+if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
+  echo "dhparams.pem does not exist - creating it"
+  openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
+fi
+
+echo "Checking for fullchain.pem"
+if [ ! -f "/etc/nginx/ssl/fullchain.pem" ]; then
+  echo "No SSL cert, enabling HTTP only..."
+  envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+else
+  echo "SSL cert exists, enabling HTTPS..."
+  envsubst < /etc/nginx/default-ssl.conf.tpl > /etc/nginx/conf.d/default.conf
+fi
+
 nginx -g 'daemon off;'
