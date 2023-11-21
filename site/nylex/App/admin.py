716641bed9d6038cc7277580ddeebe2c7b261e17
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-11-21 09:10:50 -0800

    Untrack files that are now in .gitignore.

--- site/nylex/App/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Page
-
-# Register your models here.
-
-class PageAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
-
-admin.site.register(Page, PageAdmin)
