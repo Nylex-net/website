79b1426df5863b27dd07e7e8f071055d3db5a632
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-01-26 14:14:55 -0800

    Revert to Postgres 13 and daylt ssl cert renewal.

--- site/certbot/renew.sh
+++ site/certbot/renew.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 0 3 1 * * certbot renew --dns-dnspython --dns-dnspython-credentials /opt/renewal-deSEC.ini --quiet
-echo "0 3 1 * * certbot renew --authenticator dns-desec --dns-desec-credentials /opt/renewal-deSEC.ini --quiet" > /etc/crontabs/root
+echo "0 3 * * * certbot renew --authenticator dns-desec --dns-desec-credentials /opt/renewal-deSEC.ini --quiet" > /etc/crontabs/root
 
 # Start cron in the foreground
 crond -f
