8958676fdcfe3c7210e0fde325f4b50f553b1f4d
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-09-17 16:36:40 -0700

    Validate and renew certificate in renew command file.

--- site/certbot/renew.sh
+++ site/certbot/renew.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+set -e
 
+certbot certonly \
+    --authenticator dns-desec \
+    --dns-desec-credentials /opt/deSEC.ini \
+    -d "$DOMAIN" \
+    -m "$ACME_DEFAULT_EMAIL" \
+    --agree-tos -n
+    
 # 0 3 1 * * certbot renew --dns-dnspython --dns-dnspython-credentials /opt/renewal-deSEC.ini --quiet
 echo "0 3 * * * certbot renew --authenticator dns-desec --dns-desec-credentials /opt/renewal-deSEC.ini --quiet" > /etc/crontabs/root
 
