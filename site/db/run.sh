48ffe32c68e1de736d35024085030f2ab4da452a
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-10-03 13:39:32 -0700

    It appears the database and the dump-volume aren't cooperating.  Still working on this.

--- site/db/run.sh
+++ site/db/run.sh
@@ -7,8 +7,8 @@ if [ -f /tmp/dump.dump ]; then
     echo "Restoring dump file into the database..."
 
     # Drop and recreate the database using the `postgres` administrative database
-    psql -U "$POSTGRES_USER" -d django_db -c "DROP DATABASE IF EXISTS $POSTGRES_DB;"
-    psql -U "$POSTGRES_USER" -d django_db -c "CREATE DATABASE $POSTGRES_DB;"
+    # psql -U "$POSTGRES_USER" -d django_db -c "DROP DATABASE IF EXISTS $POSTGRES_DB;"
+    # psql -U "$POSTGRES_USER" -d django_db -c "CREATE DATABASE $POSTGRES_DB;"
 
     # Restore the dump file into the newly created database
     pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" /tmp/dump.dump;
